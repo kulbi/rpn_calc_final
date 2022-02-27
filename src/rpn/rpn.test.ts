460a261e47518266153f47350be4eb4cdfcc9f9c
kulbi <kulbat.m@gmail.com>
2022-02-27 18:30:33 +0100

    all use-cases test implementation

--- src/rpn/rpn.test.ts
+++ src/rpn/rpn.test.ts
@@ -1,21 +1,27 @@
 import { rpn } from "./rpn";
-
 describe("rpn()", () => {
-    it("returs number when passed", () => {
-      expect(rpn("2")).toBe(2);
-    });
-
-
-  it("correctly adds 2 numbers", function () {
-    expect(rpn("2 0 +")).toBe(2);
+  it("returs 4 when passed", () => {
+    expect(rpn("1 3 +")).toBe(4);
   });
-
-
-  it(`correctly adds 2 numbers`, async () => {
-    expect(rpn("2 2 +")).toBe(4);
+  it("return 12 when passed", () => {
+    expect(rpn("2 2 + 3 *")).toBe(12);
   });
-
-  it("correctly divides  2  numbers", () => {
+  it("return 1 when passed", () => {
     expect(rpn("2 2 /")).toBe(1);
+  });
+  it("return 8 when passed", () => {
+    expect(rpn("3 1 - 2 2 + *")).toBe(8);
+  });
+  it("return 23 when passed", () => {
+    expect(rpn("4 3 4 + 5 1 + 2 * + +")).toBe(23);
+  });
+  it("return Invalid Expression when passed", () => {
+    expect(rpn("")).toBe("Invalid Expression");
+  });
+  it("return /Invalid Expression/ when passed", () => {
+    expect(rpn("abc")).toBe("Invalid Expression");
+  });
+  it("return message /Not Enough Operands/ when passed", () => {
+    expect(rpn("1 +")).toBe("Not Enough Operands");
   });
 });
